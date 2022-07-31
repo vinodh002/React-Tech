6576ee3ae2d7542945f4763b4645c65b9c76c860
vinodh <kvinodh102@gmail.com>
2022-07-31 21:43:41 +0530

    started main page

---
+++ src/Components/MainPage/ProductCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const ProductCard = (props) => {
+  return (
+    <div className={styled["product-card"]}>
+      <div className="styled.img">
+        <img className="product-img"></img>
+    <a></a>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
